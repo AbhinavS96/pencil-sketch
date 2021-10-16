69feed49bb67904480d0a159443ec586f2d13017
Abhinav <abhinavsidharth@gmail.com>
2021-10-16 13:57:47 +0530

    added support for color pencil sketch

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var BASE = 'http://127.0.0.2:5000';
 
-async function host() {
-	let response = await fetch(BASE + '/pencilsketch');
+async function start() {
+	let response = await fetch(BASE + '/colorpencilsketch');
 	let result = await response.json();
 	console.log(result);
 	document.querySelector('#main').src = BASE + result.path;
 }
 
-host();
+start();
--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-#save the images
-#cv2.imwrite("images/sketch.jpg", image_out[0])
-#cv2.imwrite("images/sketch-color.jpg", image_out[1])
-
 from flask import Flask, url_for
 from flask_cors import CORS, cross_origin
 import cv2
@@ -16,6 +12,7 @@ localhost = '127.0.0.2'
 my_port = '5000'
 
 
+#pencil sketch API
 @app.route('/pencilsketch')
 @cross_origin()
 def pencil():
@@ -32,6 +29,25 @@ def pencil():
     cv2.imwrite("static/pencil_sketch.jpg", image_out[0])
 
     return {'path': url_for('static', filename='pencil_sketch.jpg')}
+
+
+#color pencil sketch API
+@app.route('/colorpencilsketch')
+@cross_origin()
+def colorpencil():
+    #read the image. Currently using a local file. Will have to receive this from frontend
+    image = cv2.imread("static/image.jpg")
+
+    # creating the pencil sketch directly
+    image_out = cv2.pencilSketch(image,
+                                 sigma_s=60,
+                                 sigma_r=0.07,
+                                 shade_factor=0.05)
+
+    #save the images
+    cv2.imwrite("static/color_pencil_sketch.jpg", image_out[1])
+
+    return {'path': url_for('static', filename='color_pencil_sketch.jpg')}
 
 
 if __name__ == '__main__':
