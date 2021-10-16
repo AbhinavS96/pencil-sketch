b9ff80021d6a2ee9405b34bc3738730a5eded45c
Abhinav <abhinavsidharth@gmail.com>
2021-10-17 00:48:53 +0530

    got the posting to work.

--- main.js
+++ main.js
@@ -3,8 +3,20 @@ var BASE = 'http://127.0.0.2:5000';
 async function start() {
 	let response = await fetch(BASE + '/colorpencilsketch');
 	let result = await response.json();
-	console.log(result);
 	document.querySelector('#main').src = BASE + result.path;
 }
 
 start();
+
+async function uploadFile() {
+	let formData = new FormData();
+	formData.append("file", fileupload.files[0]);
+	let response = await fetch(BASE + '/pencilsketch', {
+		method: "POST",
+		body: formData
+	});
+	console.log(response);
+	let result = await response.json();
+	console.log(result);
+	document.querySelector('#main').src = BASE + result.path;
+}
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request, jsonify
 from flask_cors import CORS, cross_origin
 import cv2
 
 app = Flask(__name__)
 
 cors = CORS(app)
+cors = CORS(app, resources={"/pencilsketch": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 #setting custom values for sanity
@@ -13,11 +14,15 @@ my_port = '5000'
 
 
 #pencil sketch API
-@app.route('/pencilsketch')
-@cross_origin()
+@app.route('/pencilsketch', methods = ['POST'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def pencil():
-    #read the image. Currently using a local file. Will have to receive this from frontend
-    image = cv2.imread("static/image.jpg")
+    #save the posted file 
+    f = request.files['file']  
+    f.save("static/"+f.filename) 
+
+    #read the image received from frontend
+    image = cv2.imread("static/"+f.filename)
 
     # creating the pencil sketch directly
     image_out = cv2.pencilSketch(image,
@@ -25,7 +30,7 @@ def pencil():
                                  sigma_r=0.07,
                                  shade_factor=0.05)
 
-    #save the images
+    #save the image
     cv2.imwrite("static/pencil_sketch.jpg", image_out[0])
 
     return {'path': url_for('static', filename='pencil_sketch.jpg')}
